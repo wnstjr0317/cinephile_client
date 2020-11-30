0f57a8e38d15c2862a123bd1e686d994d325e2c7
chiione choi <chiionechoi@chiioneui-MacBookAir.local>
2020-11-30 14:41:03 +0900

    feat/reduxTempSetting

---
+++ client/src/components/SideBar/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<div className="nav">
+			<div className="signIn">signIn</div>
+			<div className="signUp">signUp</div>
+		</div>
+	);
+};
+
+export default Nav;
